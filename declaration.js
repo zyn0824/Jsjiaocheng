8483820bde7228a79002fb01d646b6e028df73c5
zyn0824 <17614786@qq.com>
2026-03-17 15:22:18 +0800

    fifith

---
+++ declaration.js
@@ -0,0 +1,32 @@
+// 算数运算符
+let num1 = 1 + 1;
+console.log(num1, '----------------------');
+
+let num2 = 6 / 2;
+console.log(num2, '----------------------');
+
+let num3 = 10000 % 314;  //取余
+console.log(num3, '----------------------');
+
+let num4 = 3 ** 4;  // 幂
+console.log(num4, '----------------------');
+
+// 比较运算符
+
+let x = 1;
+let y = 2;
+let bool1 = (x == y);  //全等
+console.log(bool1, '----------------------');
+
+let bool2 = (x != y);  //不等
+console.log(bool2, '----------------------');
+
+// 逻辑运算符
+let bool3 = bool1 && bool2;  // 与
+console.log(bool3, '----------------------');
+
+let bool4 = bool1 || bool2;  // 或
+console.log(bool4, '----------------------');
+
+let bool5 = !bool4; //非
+console.log(bool5, '----------------------')
---
+++ operators.js
@@ -0,0 +1,37 @@
+// 数字类型
+let year;
+year = 2026;
+let month = 3;
+const day = 20;
+var hour = 12;
+console.log(year, '++++++++++++++++');
+console.log(month, '--------------');
+console.log(day, '//////////-----');
+// console.log(hour, '//////////////');
+const int = 10;
+const float = 10.1;
+console.log(int, '//////////////');
+console.log(float, '//////////////');
+
+// 字符串类型
+const str1 = 'syxy';
+const str2 = 'blockchain';
+const str3 = str1 + str2;
+console.log(str3, '++++++++++++++++-*-*-*-');
+console.log(typeof str3, 'str3的类型');
+
+// 布尔类型
+const bool1 = true;
+const bool2 = false;
+console.log(bool1, '12121');
+console.log(bool2, '323232');
+
+// 数组类型
+const arr1 = [1, 2, 3, 4, 5];
+console.log(arr1, '数组arr1');
+console.log(typeof arr1, '************');
+
+// 对象类型
+const obj1 = {name: 'syxy', age: 18};
+console.log(obj1, '对象obj1');
+console.log(typeof obj1, '************');
